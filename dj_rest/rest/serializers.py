3c14a925006d1aba35a91375a437cdb63c3ecffb
Oleg <46264551+deKamp@users.noreply.github.com>
2020-05-20 08:46:16 +0300

    Ready

--- dj_rest/rest/serializers.py
+++ dj_rest/rest/serializers.py
@@ -14,24 +14,17 @@ class TeacherSerializer(serializers.ModelSerializer):
 
 
 class LessonsSerializer(serializers.ModelSerializer):
-    name = serializers.CharField()
-    description = serializers.CharField()
-    place = serializers.CharField()
     teacher = serializers.SerializerMethodField()
     startTime = serializers.TimeField(format='%H.%M')
     endTime = serializers.TimeField(format='%H.%M')
-    weekDay = serializers.SlugRelatedField(slug_field='id', read_only=True)
-    appointment_id = serializers.CharField()
-    service_id = serializers.CharField()
-    pay = serializers.BooleanField()
-    appointment = serializers.BooleanField()
-    teacher_v2 = TeacherSerializer()
-    color = serializers.CharField()
+    weekDay = serializers.SlugRelatedField(slug_field='id', read_only=True)    
+    teacher_v2 = TeacherSerializer()    
     availability = serializers.SlugRelatedField(slug_field='id', read_only=True)
 
     class Meta:
         model = Lesson
-        exclude = ('id',)
+        fields = ('name', 'description', 'place', 'teacher', 'startTime', 'endTime', 'weekDay', 'appointment_id',
+                 'service_id', 'pay', 'appointment', 'teacher_v2', 'color', 'availability')
 
     def get_teacher(self, obj):
         return obj.teacher_v2.short_name
